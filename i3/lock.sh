188c699b192c861270d0fb5f0015d34b7c783627
David Wesley <david.moura12@hotmail.com>
2025-04-22 15:33:03 -0300

    i3lock script

---
+++ i3/lock.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+i3lock --image=/home/david/personal/dotfiles/images/wallpaper.png --tiling --pointer=default \
+--insidever-color='#fecf4dff'   \
+--ringver-color='ffffffff'      \
+\
+--insidewrong-color='#d23c3dff' \
+--ringwrong-color='ffffffff'    \
+\
+--inside-color='#373445ff'      \
+--ring-color='#ffffffff'        \
+--separator-color='#00000000'   \
+\
+--keyhl-color='#d23c3dff'       \
+--bshl-color='#d23c3dff'        \
+\
+--line-uses-inside              \
+--radius 15                     \
+--verif-text=""                 \
+--wrong-text=""                 \
+--ind-pos="x+86:y+1003"         \
